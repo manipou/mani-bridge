9867eb43fff69825e46f068c01771456bb9d787c
Mani <71591933+manipou@users.noreply.github.com>
2025-10-09 14:09:47 +0200

    Updated Teleport Function To Have Less Skigges

--- modules/Teleport/client.lua
+++ modules/Teleport/client.lua
@@ -1,22 +1,32 @@
-local function TeleportEntity(entity, coords, transition)
-    if transition then
-        SwitchOutPlayer(entity, 0, 1)
+local function TeleportEntity(Entity, Coords, Transition)
+    if Transition then
+        SwitchOutPlayer(Entity, 0, 1)
 	    Wait(2500)
     end
 
-    if DoesEntityExist(entity) then
-        RequestCollisionAtCoord(coords.x, coords.y, coords.z)
-        while not HasCollisionLoadedAroundEntity(entity) do
-            Wait(0)
+    if DoesEntityExist(Entity) then
+        RequestCollisionAtCoord(Coords.x, Coords.y, Coords.z)
+
+        local Vehicle = cache.vehicle
+
+        if IsEntityAPed(Entity) and Vehicle then
+            SetPedCoordsKeepVehicle(Entity, Coords.x, Coords.y, Coords.z)
+            SetEntityHeading(Vehicle, Coords.w or Coords.heading or 0.0)
+        else
+            SetEntityCoords(Entity, Coords.x, Coords.y, Coords.z, false, false, false, false)
+            SetEntityHeading(Entity, Coords.w or Coords.heading or 0.0)
         end
 
-        SetEntityCoords(entity, coords.x, coords.y, coords.z, false, false, false, false)
-        SetEntityHeading(entity, coords.w or coords.heading or 0.0)
+        FreezeEntityPosition(Entity, true)
+
+        while not HasCollisionLoadedAroundEntity(Entity) do Wait(0) end
+
+        FreezeEntityPosition(Entity, false)
     end
 
-    if transition then
+    if Transition then
         Wait(2500)
-        SwitchInPlayer(entity)
+        SwitchInPlayer(Entity)
     end
 end
 
