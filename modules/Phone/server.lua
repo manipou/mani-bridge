d659a676328659f4bba63dd26338b2116de28846
Mani <71591933+manipou@users.noreply.github.com>
2025-10-09 03:36:39 +0200

    Add config check and return values to SendMessage
    SendMessage now checks if Config.Phone is set before proceeding and returns false if not. The function also returns true after successfully sending a message, and false if the phone number is not found.

--- modules/Phone/server.lua
+++ modules/Phone/server.lua
@@ -1,10 +1,12 @@
 local function SendMessage(Source, From, Message)
+    if not Config.Phone then return false end
+
     if Config.Phone == 'lb' then
         local PhoneNumber = exports["lb-phone"]:GetEquippedPhoneNumber(Source)
-        print(PhoneNumber)
-        if not PhoneNumber then return end
+        if not PhoneNumber then return false end
 
         exports['lb-phone']:SendMessage(From, PhoneNumber, Message)
+        return true
     end
 end
 
